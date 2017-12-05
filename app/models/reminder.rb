954dcba9b33351b2a64a0a256592b22b62627b0a
rhys117 <rhys117@gmail.com>
2017-12-05 22:01:21 +1000

    fixed search for PG

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -17,7 +17,7 @@ class Reminder < ApplicationRecord
 
 
 def self.search(search)
-  where("nbn_search LIKE ? OR notes LIKE ? OR service_type LIKE ? OR reference LIKE ? OR check_for LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+  where("nbn_search ILIKE ? OR notes ILIKE ? OR service_type ILIKE ? OR reference ILIKE ? OR check_for ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
 end
 
   private
