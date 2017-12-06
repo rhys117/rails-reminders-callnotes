d0957021328f62e94d20c76ffd03f53c16b1f4c2
rhys117 <rhys117@gmail.com>
2017-12-06 17:44:19 +1000

    fixing copy quick-notes

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Australia/Brisbane'
+    config.active_record.default_timezone = :local
   end
 end
