4e25f3c667cf6e0548173a58f329dbfb915f1dc9
rhys117 <rhys117@gmail.com>
2018-06-25 20:16:52 +1000

    fixed email in users after refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :correspondence, presence: true, length: {maximum: 255 },
+  validates :email, presence: true, length: {maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
   validates :sign_up_secret, inclusion: [SIGN_UP['secret']]
