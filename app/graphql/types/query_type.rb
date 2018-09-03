e318bbf2cf6f19d1e43db8b266f41625114ec222
rhys117 <rhys117@gmail.com>
2018-09-04 00:06:20 +1000

    updates to graphql

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,6 +3,10 @@ Types::QueryType = GraphQL::ObjectType.define do
 
   # queries are just represented as fields
   field :User, !types[Types::UserType] do
+    guard ->(obj, args, ctx) do
+      return unless ctx[:current_user]
+      ctx[:current_user].id == ctx[:session][:user_id]
+    end
     # resolve would be called in order to fetch data for that field
     resolve -> (obj, args, ctx) { User.all }
   end
